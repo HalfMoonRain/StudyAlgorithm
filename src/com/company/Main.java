db04070d86b67a90358bbb721d028461e8ae12e5
halfmoonrain <junwoo1357@gmail.com>
2022-05-24 17:51:21 +0900

    1-01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public int solution(String str, char t){
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        //System.out.println(str + " " + t);
+//        for(int i=0; i<str.length(); i++){
+//            if(str.charAt(i) == t) answer++;
+//        }
+        for(char x : str.toCharArray()){
+            if(x==t) answer++;
+        }
+
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+            Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+
+        System.out.println(T.solution(str,c));
     }
 }
