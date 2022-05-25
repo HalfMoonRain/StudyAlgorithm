0befa5ee225a43a8a6de7015ebb1e71e8e5e6401
halfmoonrain <junwoo1357@gmail.com>
2022-05-25 21:17:27 +0900

    ChangeFormation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,30 +2,23 @@ package com.company;
 
 import java.util.Scanner;
 
+
 public class Main {
 
-    public int solution(String str, char t){
-        int answer = 0;
-        str = str.toUpperCase();
-        t = Character.toUpperCase(t);
-        //System.out.println(str + " " + t);
-//        for(int i=0; i<str.length(); i++){
-//            if(str.charAt(i) == t) answer++;
-//        }
+    public String solution(String str){
+        String answer = "";
         for(char x : str.toCharArray()){
-            if(x==t) answer++;
+            if(x>=97 && x<=122) answer += (char)(x - 32);
+            else answer += (char)(x + 32);
         }
-
-
         return answer;
     }
 
     public static void main(String[] args) {
-            Main T = new Main();
+        Main T = new Main();
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
-        char c = kb.next().charAt(0);
+        System.out.println(T.solution(str));
 
-        System.out.println(T.solution(str,c));
     }
 }
--- src/com/company/Q1_01.java
+++
@@ -1,4 +0,0 @@
-package com.company;
-
-public class Q1_01 {
-}
