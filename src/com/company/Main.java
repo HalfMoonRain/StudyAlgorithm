db13ca1368bff32d4127457fe697e18eb7feb440
halfmoonrain <junwoo1357@gmail.com>
2022-05-26 21:55:10 +0900

    1.6.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,23 +7,13 @@ import java.util.Scanner;
 public class Main {
 
     public String solution(String str){
-        String answer;
-        char[] s = str.toCharArray();
-        int lt = 0, rt = str.length() - 1;
-        while(lt<rt){
-            if (!Character.isAlphabetic(s[lt])) {
-                lt++;
-            } else if (!Character.isAlphabetic(s[rt])) {
-                rt--;
-            } else {
-                char tmp = s[lt];
-                s[lt] = s[rt];
-                s[rt]=tmp;
-                lt++;
-                rt--;
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+        //    System.out.println(str.charAt(i)+""+i+""+str.indexOf(str.charAt(i)));
+            if(str.indexOf(str.charAt(i))==i){
+                answer += str.charAt(i);
             }
         }
-        answer = String.valueOf(s);
         return answer;
     }
 
