899f80f353c9d4f1f5c605bc9ed79419c4325a30
halfmoonrain <junwoo1357@gmail.com>
2022-05-27 21:57:35 +0900

    1.7.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,10 @@ import java.util.Scanner;
 public class Main {
 
     public String solution(String str){
-        String answer = "YES";
-        str = str.toUpperCase();
-        int len = str.length();
-        for (int i = 0; i < len / 2; ++i) {
-            if (str.charAt(i) != str.charAt(len - i - 1)) {
-                answer = "NO";
-            }
+        String answer = "NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equals(tmp)) {
+            answer = "YES";
         }
         return answer;
     }
