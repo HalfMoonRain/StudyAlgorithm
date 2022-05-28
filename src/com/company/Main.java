37e0bc0a7bb5a88674f0dce74904b6528e115810
halfmoonrain <junwoo1357@gmail.com>
2022-05-28 18:19:59 +0900

    1.9.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
 public class Main {
 
     public int solution(String s){
-        int answer = 0;
+        String answer = "";
         for(char x : s.toCharArray()){
-            if(x>48 && x <=57) answer = answer * 10 + (x - 48);
+            if(Character.isDigit(x)) answer += x;
         }
-        return answer;
+        return Integer.parseInt(answer);
     }
 
     public static void main(String[] args) {
